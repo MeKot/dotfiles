7150c240e986fecf54c00d4cd6e942b3ce3d1b20
admin <admin@nixoslocal.com>
2024-05-16 21:50:36 +0100

    flaking

---
+++ nixos/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Sysme Config";
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+
+      inherit system;
+      config = { allowUnfree = true };
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        
+	inherit system;
+	modules = [
+	  ./system/configuration.nix
+	];
+      };
+
+    };
+  };
+}
